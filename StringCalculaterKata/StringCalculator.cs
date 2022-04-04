8bb7146239064326cc5b8b2d2cdce43aa5c06396
R-abodyak <rawanabodiak12@gmail.com>
2022-04-04 14:41:18 +0300

    step1 : Green for two argument

--- StringCalculaterKata/StringCalculator.cs
+++ StringCalculaterKata/StringCalculator.cs
@@ -7,7 +7,11 @@ namespace StringCalculator.Core
         {
             if (v.Length == 0) return 0;
             if (v.Length == 1) return int.Parse(v);
-            throw new NotImplementedException();
+            string[] numbers = v.Split(",");
+            int number1 = int.Parse(numbers[0]);
+            int number2 = int.Parse(numbers[1]);
+            return number1 + number2;
+          
         }
     }
 }
