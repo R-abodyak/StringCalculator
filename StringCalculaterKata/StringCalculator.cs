1fbfb1ea809bd89d46f560d64909306a23602dcb
R-abodyak <rawanabodiak12@gmail.com>
2022-04-04 14:27:09 +0300

    STEP1 : make green test for emty string

--- StringCalculaterKata/StringCalculator.cs
+++ StringCalculaterKata/StringCalculator.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-
 namespace StringCalculator.Core
 {
     public class StringCalculator
     {
         public int add(string v)
         {
+            if (v.Length == 0) return 0;
             throw new NotImplementedException();
         }
     }
